a70e7f444b8f89fe3840e6556612cbf6aaf2510d
sunil <sunil@gmail.com>
2025-12-18 13:21:20 +0530

    VM

---
+++ LeaveManagement/VM/SalaryAdjustmentVM.cs
@@ -0,0 +1,17 @@
+﻿using LeaveManagement.Models;
+
+namespace LeaveManagement.VM
+{
+    public class SalaryAdjustmentVM
+    {
+        public ApplicationUser Employee { get; set; }
+        public List<LeaveRequest> PreviousMonthLeaves { get; set; }
+        public DateTime PreviousMonthStart { get; set; }
+        public DateTime PreviousMonthEnd { get; set; }
+        public int TotalWorkingDays { get; set; }   
+        public double LeavesTaken { get; set; }
+        public double FreeLeavesAvailable { get; set; }
+        public List<LeaveAdjustmentHistory> History { get; set; }
+
+    }
+}
